8072a88b404d89625f67847bb39f3c37391a3d08
Sungil.Hong <suh33@pitt.edu>
2020-09-13 00:46:36 -0400

    Add python3-pytest in ubuntu-setup.sh and wsl-ubuntu.sh

--- module-dev-env/wsl-ubuntu.sh
+++ module-dev-env/wsl-ubuntu.sh
@@ -3,7 +3,7 @@ set -ex
 
 apt-get update
 apt-get install -y curl vim
-apt-get install -y build-essential python3 python3-pip python3-venv
+apt-get install -y build-essential python3 python3-pip python3-venv python3-pytest
 
 
 # install atom: https://atom.io/
